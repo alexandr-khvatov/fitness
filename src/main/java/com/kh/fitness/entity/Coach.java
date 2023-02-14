079846f4c79dcb5de655974cfa61a5e3e9869882
alexandr-khvatov <alexkhvatov.dev@gmail.com>
2023-02-14 16:58:01 +0400

    refactor entity layer

--- src/main/java/com/kh/fitness/entity/Coach.java
+++ src/main/java/com/kh/fitness/entity/Coach.java
@@ -1,17 +1,14 @@
 package com.kh.fitness.entity;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
 import javax.persistence.*;
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Set;
 
 import static javax.persistence.FetchType.LAZY;
 
-@EqualsAndHashCode(exclude = {"trainings"})
 @ToString(exclude = {"trainings"})
 @Data
 @Entity
@@ -36,4 +33,80 @@ public class Coach implements BaseEntity<Long> {
 
     @OneToMany(mappedBy = "coach")
     private Set<Training> trainings;
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Coach)) return false;
+        final Coach other = (Coach) o;
+        if (!other.canEqual((Object) this)) return false;
+        final Object this$id = this.getId();
+        final Object other$id = other.getId();
+        if (this$id == null ? other$id != null : !this$id.equals(other$id)) return false;
+        final Object this$firstname = this.getFirstname();
+        final Object other$firstname = other.getFirstname();
+        if (this$firstname == null ? other$firstname != null : !this$firstname.equals(other$firstname)) return false;
+        final Object this$patronymic = this.getPatronymic();
+        final Object other$patronymic = other.getPatronymic();
+        if (this$patronymic == null ? other$patronymic != null : !this$patronymic.equals(other$patronymic))
+            return false;
+        final Object this$lastname = this.getLastname();
+        final Object other$lastname = other.getLastname();
+        if (this$lastname == null ? other$lastname != null : !this$lastname.equals(other$lastname)) return false;
+        final Object this$birthDate = this.getBirthDate();
+        final Object other$birthDate = other.getBirthDate();
+        if (this$birthDate == null ? other$birthDate != null : !this$birthDate.equals(other$birthDate)) return false;
+        final Object this$phone = this.getPhone();
+        final Object other$phone = other.getPhone();
+        if (this$phone == null ? other$phone != null : !this$phone.equals(other$phone)) return false;
+        final Object this$email = this.getEmail();
+        final Object other$email = other.getEmail();
+        if (this$email == null ? other$email != null : !this$email.equals(other$email)) return false;
+        final Object this$specialization = this.getSpecialization();
+        final Object other$specialization = other.getSpecialization();
+        if (this$specialization == null ? other$specialization != null : !this$specialization.equals(other$specialization))
+            return false;
+        final Object this$image = this.getImage();
+        final Object other$image = other.getImage();
+        if (this$image == null ? other$image != null : !this$image.equals(other$image)) return false;
+        final Object this$description = this.getDescription();
+        final Object other$description = other.getDescription();
+        if (this$description == null ? other$description != null : !this$description.equals(other$description))
+            return false;
+        final Object this$gym = this.getGym();
+        final Object other$gym = other.getGym();
+        if (this$gym == null ? other$gym != null : !this$gym.equals(other$gym)) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof Coach;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $id = this.getId();
+        result = result * PRIME + ($id == null ? 43 : $id.hashCode());
+        final Object $firstname = this.getFirstname();
+        result = result * PRIME + ($firstname == null ? 43 : $firstname.hashCode());
+        final Object $patronymic = this.getPatronymic();
+        result = result * PRIME + ($patronymic == null ? 43 : $patronymic.hashCode());
+        final Object $lastname = this.getLastname();
+        result = result * PRIME + ($lastname == null ? 43 : $lastname.hashCode());
+        final Object $birthDate = this.getBirthDate();
+        result = result * PRIME + ($birthDate == null ? 43 : $birthDate.hashCode());
+        final Object $phone = this.getPhone();
+        result = result * PRIME + ($phone == null ? 43 : $phone.hashCode());
+        final Object $email = this.getEmail();
+        result = result * PRIME + ($email == null ? 43 : $email.hashCode());
+        final Object $specialization = this.getSpecialization();
+        result = result * PRIME + ($specialization == null ? 43 : $specialization.hashCode());
+        final Object $image = this.getImage();
+        result = result * PRIME + ($image == null ? 43 : $image.hashCode());
+        final Object $description = this.getDescription();
+        result = result * PRIME + ($description == null ? 43 : $description.hashCode());
+        final Object $gym = this.getGym();
+        result = result * PRIME + ($gym == null ? 43 : $gym.hashCode());
+        return result;
+    }
 }
