755aa50ee4a00b31a8fb5f30142a3dc76290ed48
alexandr-khvatov <alexkhvatov.dev@gmail.com>
2023-02-26 20:58:23 +0400

    update data for test

---
+++ src/test/resources/sql/clear-test-data.sql
@@ -0,0 +1,13 @@
+TRUNCATE  TABLE
+    free_pass,
+    subscription,
+    training,
+    room,
+    sub_training_program,
+    training_program,
+    coach,
+    users_role,
+    users,
+    role  ,
+    gym_opening_hour_info,
+    gym CASCADE ;
