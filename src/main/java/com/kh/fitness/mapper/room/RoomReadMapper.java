a0457b357eff33ddc565b27143bb55688c60c27b
alexandr-khvatov <alexkhvatov.dev@gmail.com>
2023-02-27 01:12:12 +0400

    refactor

--- src/main/java/com/kh/fitness/mapper/room/RoomReadMapper.java
+++ src/main/java/com/kh/fitness/mapper/room/RoomReadMapper.java
@@ -3,8 +3,10 @@ package com.kh.fitness.mapper.room;
 import com.kh.fitness.dto.room.RoomReadDto;
 import com.kh.fitness.entity.Room;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface RoomReadMapper {
+    @Mapping(target = "gymId",source = "gym.id")
     RoomReadDto toDto(Room s);
 }
