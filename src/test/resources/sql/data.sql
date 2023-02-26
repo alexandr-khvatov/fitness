9efff96b9e2b615f61c88ea1173bf2fdde4d97ef
alexandr-khvatov <alexkhvatov.dev@gmail.com>
2023-02-26 17:36:31 +0400

    added test data.sql

--- src/test/resources/sql/data.sql
+++ src/test/resources/sql/data.sql
@@ -1,163 +1,176 @@
-INSERT INTO gym (name, address, email, phone, working_hours_on_weekdays, working_hours_on_weekends, inst_link, tg_link,
-                 vk_link)
-VALUES ('СТАРТ.ПОЛИТЕХ',
+INSERT INTO gym (id,name, address, email, phone, inst_link, tg_link, vk_link, min_start_time, max_end_time)
+VALUES (1,
+        'СТАРТ.ПОЛИТЕХ',
         'Политехническая улица, 122А',
         'start@mail.ru',
         '+78452000000',
-        '8:00-21:00',
-        '8:00-21:00',
         'https://instagram.com/start',
         'https://tg.com/start',
-        'https://vk.com/start');
+        'https://vk.com/start',
+        '8:00:00',
+        '21:00:00');
+SELECT SETVAL('gym_id_seq', (SELECT MAX(id) FROM gym));
 
-INSERT INTO role (name)
-VALUES (UPPER('ADMIN')),
-       (UPPER('MANAGER')),
-       (UPPER('CUSTOMER'));
+INSERT INTO gym_opening_hour_info (gym_id, day_of_week, start_time, end_time, is_open)
+VALUES (1, 0, '8:00:00', '21:00:00', TRUE),
+       (1, 1, '8:00:00', '21:00:00', TRUE),
+       (1, 2, '8:00:00', '21:00:00', TRUE),
+       (1, 3, '8:00:00', '21:00:00', TRUE),
+       (1, 4, '8:00:00', '21:00:00', TRUE),
+       (1, 5, '8:00:00', '21:00:00', TRUE),
+       (1, 6, '8:00:00', '21:00:00', TRUE);
 
-SELECT *
-FROM role;
+INSERT INTO role (id,name)
+VALUES (1,UPPER('ADMIN')),
+       (2,UPPER('MANAGER')),
+       (3,UPPER('CUSTOMER'));
+SELECT SETVAL('role_id_seq', (SELECT MAX(id) FROM role));
 
-INSERT INTO users (firstname, patronymic, lastname, email, password, phone, birth_date)
-VALUES ('Иван', 'Иванович', 'Иванов', 'admin@gmail.com', '{noop}6%J#Z%u4GgiTv5BE4', '+70000000001',
+INSERT INTO users (id,firstname, patronymic, lastname, email, password, phone, birth_date)
+VALUES (1,'Иван', 'Бунин', 'Иванов', 'admin@gmail.com', '{noop}6%J#Z%u4GgiTv5BE4', '+70000000001',
         '01.01.1991'),
-       ('Анна', 'Олеговна', 'Еськина', 'manager@gmail.com', '{noop}6%J#Z%u4GgiTv5BE4', '+70000000002',
+       (2,'Анна', 'Ахматова', 'Еськина', 'manager@gmail.com', '{noop}6%J#Z%u4GgiTv5BE4', '+70000000002',
         '01.01.1991'),
-       ('Федор', 'Иванович', 'Тютчев', 'customer@gmail.com', '{noop}6%J#Z%u4GgiTv5BE4', '+70000000004',
+       (3,'Федор', 'Достоевский', 'Тютчев', 'customer@gmail.com', '{noop}6%J#Z%u4GgiTv5BE4', '+70000000004',
         '01.01.1991');
-
-SELECT *
-FROM users;
+SELECT SETVAL('users_id_seq', (SELECT MAX(id) FROM users));
 
 INSERT INTO users_role (user_id, role_id)
 VALUES ((SELECT id FROM users WHERE email = 'admin@gmail.com'),
         (SELECT id FROM role WHERE UPPER(name) = UPPER('ADMIN'))),
+       ((SELECT id FROM users WHERE email = 'admin@gmail.com'),
+        (SELECT id FROM role WHERE UPPER(name) = UPPER('CUSTOMER'))),
+
        ((SELECT id FROM users WHERE email = 'manager@gmail.com'),
         (SELECT id FROM role WHERE UPPER(name) = UPPER('MANAGER'))),
+       ((SELECT id FROM users WHERE email = 'manager@gmail.com'),
+        (SELECT id FROM role WHERE UPPER(name) = UPPER('CUSTOMER'))),
+
        ((SELECT id FROM users WHERE email = 'customer@gmail.com'),
         (SELECT id FROM role WHERE UPPER(name) = UPPER('CUSTOMER')));
 
-INSERT INTO coach (gym_id, firstname, patronymic, lastname, birth_date, phone, email, specialization, description,
+INSERT INTO coach (id,gym_id, firstname, patronymic, lastname, birth_date, phone, email, specialization, description,
                    image)
-VALUES (1, 'Александр ',
-        'Иванович',
-        'Белов',
+VALUES (1,1, 'Александр',
+        'Александрович',
+        'Блок',
         '01.01.1991',
         '+70000000001',
         'coach1@gmail.com',
         'Фитнес – менеджер',
         'Инструктор групповых программ HOT IRON, CYCLING. Здоровый человек – счастливый человек!',
         NULL),
-       (1, 'Анна', 'Иванович', 'Еськина',
+       (2,1, 'Вера', 'Михайловна', 'Инбер',
         '01.01.1991',
         '+70000000002',
         'coach2@gmail.com',
         'Персональный тренер, инструктор тренажерного зала',
         'Инструктор групповых программ HOT IRON, CYCLING. Здоровый человек – счастливый человек!',
         NULL),
-       (1, 'Кирилл', 'Иванович', 'Алексеев ',
+       (3,1, 'Сергей', 'Александрович', 'Есенин',
         '01.01.1991',
         '+70000000003',
         'coach3@gmail.com',
         'Персональный тренер, инструктор тренажерного зала',
         'Инструктор групповых программ HOT IRON, CYCLING. Здоровый человек – счастливый человек!',
         NULL),
-       (1, 'Неля ', 'Иванович', 'Стадник',
+       (4,1, 'Неля ', 'Иванович', 'Стадник',
         '01.01.1991',
         '+70000000004',
         'coach4@gmail.com',
         'Персональный тренер, инструктор тренажерного зала',
         'Инструктор групповых программ HOT IRON, CYCLING. Здоровый человек – счастливый человек!',
         NULL),
-       (1, 'Неля ', 'Иванович', 'Стадник',
+       (5,1, 'Белла', 'Ахатовна', 'Ахмадулина',
         '01.01.1991',
         '+70000000005',
         'coach5@gmail.com',
         'Персональный тренер, инструктор тренажерного зала',
         'Инструктор групповых программ HOT IRON, CYCLING. Здоровый человек – счастливый человек!',
         NULL),
-       (1, 'Неля ', 'Иванович', 'Беспалова',
+       (6,1, 'Агния', 'Львовна', 'Барто',
         '01.01.1991',
         '+70000000006',
         'coach6@gmail.com',
         'Персональный тренер, инструктор тренажерного зала',
         'Инструктор групповых программ HOT IRON, CYCLING. Здоровый человек – счастливый человек!',
         NULL),
-       (1, 'Ольга', 'Иванович', 'Глаголева ',
+       (7,1, 'Ольга', 'Федоровна', 'Берггольц',
         '01.01.1991',
         '+70000000007',
         'coach7@gmail.com',
         'тренер',
         'Инструктор групповых программ HOT IRON, CYCLING. Здоровый человек – счастливый человек!',
         '06caf993-a9b4-4082-a7d0-c1b74cb0b941.jpg');
+SELECT SETVAL('coach_id_seq', (SELECT MAX(id) FROM coach));
 
-INSERT INTO training_program (gym_id, name, description, overview, image)
-VALUES (1, 'СИЛОВЫЕ ТРЕНИРОВКИ',
+INSERT INTO training_program (id,gym_id, name, description, overview, image)
+VALUES (1,1, 'СИЛОВЫЕ ТРЕНИРОВКИ',
         'Групповые тренировки, направленные на развитие силы, выносливости и укрепления всех групп мышц. Мы предлагаем следующие направления силовых тренировок',
         'PumpSuper sculptLower bodyUpper workBums+ABS',
         NULL),
-       (1, 'BODY&MIND ТРЕНИРОВКИ',
+       (2,1, 'BODY&MIND ТРЕНИРОВКИ',
         'Универсальная групповая программа тренировок, которая подойдет всем, включая новичков. Мы предлагаем следующие направления body&mind тренировок',
         'ПилатесБалансЙогаЙога-ланчЙога-здоровая спинаЙога в гамакахЙога в гамакахMFR+Stretching',
         NULL),
-       (1, 'ФУНКЦИОНАЛЬНЫЕ ТРЕНИРОВКИ',
+       (3,1, 'ФУНКЦИОНАЛЬНЫЕ ТРЕНИРОВКИ',
         'Групповые занятия, которые помогут развить ловкость, силу, координацию и выносливость. Регулярные занятия дадут вам возможность получить физически развитое и красивое тело. Мы предлагаем следующие направления функциональных тренировок:',
         'Functional training',
         NULL),
-       (1, 'СМЕШАННЫЕ ТРЕНИРОВКИ',
+       (4,1, 'СМЕШАННЫЕ ТРЕНИРОВКИ',
         'Групповые занятия, основанные на комбинированных тренировках - это одновременное сочетание силовых упражнений с аэробными нагрузками. Помогут значительно улучшить физическую форму. Мы предлагаем следующие направления смешанных тренировок',
         'ABS+stretching',
         NULL),
-       (1, 'ТАНЦЕВАЛЬНЫЕ ТРЕНИРОВКИ',
+       (5,1, 'ТАНЦЕВАЛЬНЫЕ ТРЕНИРОВКИ',
         'Групповые тренировки, которые сочетают в себе аэробные и кардио нагрузки. Эффективно сжигают лишние калории, избавляют от лишнего веса и делают осанку и тело красивее. Мы предлагаем следующие направления танцевальных тренировок',
         'Belly danceBelly PROZumba',
         NULL),
-       (1, 'КАРДИО ТРЕНИРОВКИ',
+       (6,1, 'КАРДИО ТРЕНИРОВКИ',
         'Универсальная групповая программа тренировок для укрепления сердечной мышцы, а также улучшения состояния систем кровообращения и дыхания. Отлично подходят для похудения. Мы предлагаем следующие направления кардио-тренировок',
         'Степ PRO',
         NULL);
+SELECT SETVAL('training_program_id_seq', (SELECT MAX(id) FROM training_program));
 
-INSERT INTO sub_training_program (name, training_program_id, overview, description, image)
-VALUES ('Pump', 1, 'overview1', 'description1', NULL),
-       ('Super sculpt', 1, 'overview1', 'description1', NULL),
-       ('Lower body', 1, 'overview1', 'description1', NULL),
-       ('Йога', 2, 'overview2', 'description2', NULL),
-       ('Йога-ланч', 2, 'overview2', 'description2', NULL),
-       ('Пилатес', 2, 'overview2', 'description2', NULL),
-       ('MFR+Stretching', 2, 'overview2', 'description2', NULL),
-       ('Functional training', 3, 'overview3', 'description3', NULL),
-       ('Степ PRO', 4, 'overview4', 'description4', NULL),
-       ('ABS+stretching', 5, 'overview5', 'description5', NULL),
-       ('Belly dance', 6, 'overview6', 'description6', NULL),
-       ('Belly PRO', 6, 'overview6', 'description6', NULL),
-       ('Zumba', 6, 'overview6', 'description6', NULL);
+INSERT INTO sub_training_program (id,name, training_program_id, overview, description, image)
+VALUES (1,'Pump', 1, 'overview1', 'description1', NULL),
+       (2,'Super sculpt', 1, 'overview1', 'description1', NULL),
+       (3,'Lower body', 1, 'overview1', 'description1', NULL),
+       (4,'Йога', 2, 'overview2', 'description2', NULL),
+       (5,'Йога-ланч', 2, 'overview2', 'description2', NULL),
+       (6,'Пилатес', 2, 'overview2', 'description2', NULL),
+       (7,'MFR+Stretching', 2, 'overview2', 'description2', NULL),
+       (8,'Functional training', 3, 'overview3', 'description3', NULL),
+       (9,'Степ PRO', 4, 'overview4', 'description4', NULL),
+       (10,'ABS+stretching', 5, 'overview5', 'description5', NULL),
+       (11,'Belly dance', 6, 'overview6', 'description6', NULL),
+       (12,'Belly PRO', 6, 'overview6', 'description6', NULL),
+       (13,'Zumba', 6, 'overview6', 'description6', NULL);
+SELECT SETVAL('training_program_id_seq', (SELECT MAX(id) FROM training_program));
 
-INSERT INTO training (gym_id, coach_id, name, start_time, end_time, day_of_week)
-VALUES (1, 1, 'Yoga', '10:00', '11:00', '1'),
-       (1, 1, 'Tabata', '10:00', '11:00', '2'),
-       (1, 1, 'Stretch', '10:00', '11:00', '3'),
-       (1, 1, 'Yoga', '10:00', '11:00', '4'),
-       (1, 1, 'Body Sculpt', '10:00', '11:00', '5'),
-       (1, 1, 'Belly Dance', '10:00', '11:00', '6'),
-       (1, 1, 'Pilates', '10:00', '11:00', '7');
-
-INSERT INTO subscription (name, gym_id, period, price)
-VALUES ('1 месяц', 1, '1 месяц', '3500'),
-       ('3 месяца', 1, '3 месяца', '7700'),
-       ('6 месяцев', 1, '6 месяцев', '13800'),
-       ('12 месяц', 1, '12 месяц', '23300');
+INSERT INTO room (id,gym_id, name)
+VALUES (1,'1', 'Большой зал'),
+       (2,'1', 'Малый зал');
+SELECT SETVAL('room_id_seq', (SELECT MAX(id) FROM room));
 
 
-INSERT INTO free_pass (gym_id,email, firstname, lastname, phone, is_done)
-VALUES (1,'test@gmail.com', 'Иван', 'Иванов', '+79990009999', FALSE),
-       (1,'test1@gmail.com', 'Алексей', 'Иванов', '+79991009999', FALSE),
-       (1,'test21@gmail.com', 'Алексей', 'Иванов', '+79992009999', FALSE),
-       (1,'test31@gmail.com', 'Алексей', 'Иванов', '+79993009999', FALSE),
-       (1,'test41@gmail.com', 'Алексей', 'Иванов', '+79994009999', FALSE),
-       (1,'test51@gmail.com', 'Алексей', 'Иванов', '+79995009999', FALSE),
-       (1,'test61@gmail.com', 'Алексей', 'Иванов', '+79996009999', FALSE),
-       (1,'test71@gmail.com', 'Алексей', 'Иванов', '+79997009999', FALSE),
-       (1,'test81@gmail.com', 'Алексей', 'Иванов', '+79998009999', FALSE),
-       (1,'test2@gmail.com', 'Игнат', 'Иванов', '+79990109999', FALSE);
+INSERT INTO training (id,gym_id, coach_id, room_id, sub_training_program_id, name, start_time, end_time, day_of_week,
+                      total_seats, taken_seats)
+VALUES (1,1, 1, '1', '1', 'Yoga', '09:00:00', '10:00:00', '1', '25', '0'),
+       (2,1, 1, '1', '1', 'Tabata', '10:00:00', '11:10:00', '2', '25', '0'),
+       (3,1, 1, '1', '1', 'Stretch', '11:00:00', '12:10:00', '3', '25', '0'),
+       (4,1, 1, '2', '1', 'Yoga', '12:00:00', '13:10:00', '4', '25', '0'),
+       (5,1, 1, '2', '1', 'Body Sculpt', '13:00:00', '14:10:00', '5', '25', '0'),
+       (6,1, 1, '2', '1', 'Belly Dance', '14:00:00', '15:10:00', '6', '25', '0'),
+       (7,1, 1, '2', '1', 'Pilates', '15:00:00', '16:10:00', '0', '25', '0');
+SELECT SETVAL('training_id_seq', (SELECT MAX(id) FROM training));
 
+INSERT INTO subscription (id,name, gym_id, period, price)
+VALUES (1,'1 месяц', 1, '1 месяц', '3500'),
+       (2,'3 месяца', 1, '3 месяца', '7700'),
+       (3,'6 месяцев', 1, '6 месяцев', '13800'),
+       (4,'12 месяц', 1, '12 месяц', '23300');
+SELECT SETVAL('subscription_id_seq', (SELECT MAX(id) FROM subscription));
 
+INSERT INTO free_pass (id,gym_id, training_id, date, start_time, end_time, email, firstname, lastname, phone, is_done)
+VALUES (1,1, 1, '2022-12-13', '19:00:00', '20:00:00', 'test@gmail.com', 'Иван', 'Иванов', '+79990009999', FALSE);
+SELECT SETVAL('free_pass_id_seq', (SELECT MAX(id) FROM free_pass));
