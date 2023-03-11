11f502044d90e4a6077f7684d6574530fff275d2
alexandr-khvatov <alexkhvatov.dev@gmail.com>
2023-03-11 22:37:26 +0400

    update freepass

--- src/main/java/com/kh/fitness/api/FreePassController.java
+++ src/main/java/com/kh/fitness/api/FreePassController.java
@@ -1,27 +1,32 @@
 package com.kh.fitness.api;
 
 import com.kh.fitness.api.util.PathUtils;
-import com.kh.fitness.dto.free_pass.FreePassEditTrainingDto;
-import com.kh.fitness.dto.free_pass.FreePassReadDto;
 import com.kh.fitness.dto.free_pass.FreePassCreateDto;
-import com.kh.fitness.service.FreePassServiceImpl;
+import com.kh.fitness.dto.free_pass.FreePassEditDto;
+import com.kh.fitness.dto.free_pass.FreePassReadDto;
+import com.kh.fitness.service.free_pass.FreePassServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Optional;
+
+import static java.lang.String.format;
 
 @RestController
 @RequestMapping(PathUtils.API_V1 + "/free-pass")
 @RequiredArgsConstructor
-public class FreePassRequestController {
+public class FreePassController {
     private final FreePassServiceImpl freePassRequestService;
 
+    private static final String ERROR_MSG_NOT_FOUND = "Free pass with id %s not found";
+
     @GetMapping("/{id}")
-    public Optional<FreePassReadDto> findById(@PathVariable Long id) {
-        return freePassRequestService.findById(id);
+    public FreePassReadDto findById(@PathVariable Long id) {
+        return freePassRequestService.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, format(ERROR_MSG_NOT_FOUND, id))
+        );
     }
 
     @GetMapping
@@ -35,20 +40,22 @@ public class FreePassRequestController {
     }
 
     @PutMapping("/{id}")
-    public Optional<FreePassReadDto> update(@PathVariable Long id) {
-        return freePassRequestService.updateFieldIsDone(id);
+    public FreePassReadDto update(@PathVariable Long id) {
+        return freePassRequestService.updateFieldIsDone(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, format(ERROR_MSG_NOT_FOUND, id))
+        );
     }
 
     @PutMapping("/change/{id}")
-    public Optional<FreePassReadDto> changeTraining(@PathVariable Long id,@RequestBody FreePassEditTrainingDto dto) {
-        return freePassRequestService.changeTraining(id,dto);
+    public FreePassReadDto changeTraining(@PathVariable Long id, @RequestBody FreePassEditDto dto) {
+        return freePassRequestService.changeTraining(id, dto);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
         if (Boolean.FALSE.equals(freePassRequestService.delete(id))) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, format(ERROR_MSG_NOT_FOUND, id));
         }
     }
 }
